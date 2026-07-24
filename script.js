1ce361639b5ea3dc45af6f72de5f6b14756532c6
sidrajamshaid <sidrajamshaid.nabeeha@gmail.com>
2026-07-24 13:32:36 +0200

    Added functionality toabout and contact button using java script

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuButton = document.getElementById("menu-button");
 const closeButton = document.getElementById("close-button");
 const mobileMenu = document.getElementById("mobile-menu");
+const mobileContactButton = document.getElementById("mobile-contact-button");
+const mobileAboutButton= document.getElementById("mobile-about-button");
 
 menuButton.addEventListener("click", function () {
   mobileMenu.classList.add("show");
@@ -8,4 +10,14 @@ menuButton.addEventListener("click", function () {
 
 closeButton.addEventListener("click", function () {
   mobileMenu.classList.remove("show");
+});
+
+mobileContactButton.addEventListener("click", function () {
+  mobileMenu.classList.remove("show");
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
+
+mobileAboutButton.addEventListener("click", function () {
+  mobileMenu.classList.remove("show");
+  document.getElementById("mobile-about-button").scrollIntoView({ behavior: "smooth" });
 });
